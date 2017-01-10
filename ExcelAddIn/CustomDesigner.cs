20d2b77dc08c611d920c0053d74e18f249d0b846
v-tazho@hotmail.com <Edward1121>
2017-01-10 13:25:25 +0800

    first check in

---
+++ ExcelAddIn/CustomDesigner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace ExcelAddIn
+{
+    public partial class CustomDesigner
+    {
+        private void CustomDesigner_Load(object sender, RibbonUIEventArgs e)
+        {
+            this.tab1.KeyTip = "z";
+            //this.button1.KeyTip = "cks1";
+        }
+    }
+}
---
+++ OutlookAddIn/RibbonDesigner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace OutlookAddIn
+{
+    public partial class RibbonDesigner
+    {
+        private void RibbonDesigner_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void HideAttachFile_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
