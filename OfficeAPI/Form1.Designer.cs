807e1988abd132e6667709425fbd002ec5250907
Edward-Zhou <v-tazho@hotmail.com>
2017-01-11 09:48:46 +0800

    20170110

---
+++ OfficeAPI/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace OfficeAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.WordToHtml = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.WordToHtml);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(906, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "OpenXML";
+            // 
+            // WordToHtml
+            // 
+            this.WordToHtml.Location = new System.Drawing.Point(7, 20);
+            this.WordToHtml.Name = "WordToHtml";
+            this.WordToHtml.Size = new System.Drawing.Size(75, 23);
+            this.WordToHtml.TabIndex = 0;
+            this.WordToHtml.Text = "WordToHtml";
+            this.WordToHtml.UseVisualStyleBackColor = true;
+            this.WordToHtml.Click += new System.EventHandler(this.WordToHtml_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 453);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button WordToHtml;
+    }
+}
+
---
+++ OfficeAPI/OpenXMLAPI/ConvertWordToHtml.cs
@@ -0,0 +1,115 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OfficeAPI.OpenXMLAPI
+{
+    public class ConvertWordToHtml:IButtonClick
+    {
+        
+        public static void WordToHtml()
+        {
+            byte[] byteArray = File.ReadAllBytes(@"D:\OfficeDev\Word\201701\Test.docx");
+            string css = @"
+        p.PtNormal
+            {margin-bottom:10.0pt;
+            font-size:11.0pt;
+            font-family:""Times"";}
+        span.PtDefaultParagraphFont
+            {margin-top:24.0pt;
+            font-size:14.0pt;
+            font-family:""Helvetica"";
+            color:yellow;}
+        h1.PtHeading1
+            {margin-top:24.0pt;
+            font-size:14.0pt;
+            font-family:""Helvetica"";
+            color:blue;}
+        h2.PtHeading2
+            {margin-top:10.0pt;
+            font-size:13.0pt;
+            font-family:""Helvetica"";
+            color:blue;}";
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+                using (WordprocessingDocument doc = WordprocessingDocument.Open(memoryStream, true))
+                {
+                    int imageCounter = 0;
+                    HtmlConverterSettings settings = new HtmlConverterSettings()
+                    {
+                        PageTitle = "My Page Title",
+                        
+                        CssClassPrefix = "Pt",
+                        AdditionalCss= css,
+                        ImageHandler = imageInfo =>
+                        {
+                            DirectoryInfo localDirInfo = new DirectoryInfo(@"D:\OfficeDev\Word\201701\img");
+                            if (!localDirInfo.Exists)
+                            {
+                                localDirInfo.Create();
+                            }
+                            ++imageCounter;
+                            string extension = imageInfo.ContentType.Split('/')[1].ToLower();
+                            ImageFormat imageFormat = null;
+                            if (extension == "png")
+                            {
+                                extension = "gif";
+                                imageFormat = ImageFormat.Gif;
+                            }
+                            else if (extension == "gif")
+                                imageFormat = ImageFormat.Gif;
+                            else if (extension == "bmp")
+                                imageFormat = ImageFormat.Bmp;
+                            else if (extension == "jpeg")
+                                imageFormat = ImageFormat.Jpeg;
+                            else if (extension == "tiff")
+                            {
+                                extension = "gif";
+                                imageFormat = ImageFormat.Gif;
+                            }
+                            else if (extension == "x-wmf")
+                            {
+                                extension = "wmf";
+                                imageFormat = ImageFormat.Wmf;
+                            }
+                            if (imageFormat == null)
+                                return null;
+
+                            string imageFileName = @"D:\OfficeDev\Word\201701\image" +
+                                imageCounter.ToString() + "." + extension;
+                            try
+                            {
+                                imageInfo.Bitmap.Save(imageFileName, imageFormat);
+                            }
+                            catch (System.Runtime.InteropServices.ExternalException)
+                            {
+                                return null;
+                            }
+                            XElement img = new XElement(Xhtml.img,
+                                new XAttribute(NoNamespace.src, imageFileName),
+                                imageInfo.ImgStyleAttribute,
+                                imageInfo.AltText != null ?
+                                    new XAttribute(NoNamespace.alt, imageInfo.AltText) : null);
+                            return img;
+                        }
+                    };
+                    XElement html = HtmlConverter.ConvertToHtml(doc, settings);
+                    File.WriteAllText(@"D:\OfficeDev\Word\201701\WordHtml.html", html.ToStringNewLineOnAttributes());
+                };
+            }
+        }
+
+        public void MessageBox(string message)
+        {
+            System.Windows.Forms.MessageBox.Show(message);
+        }
+    }
+}
